3d5ac222e549b81ee247809e4b8153febfa43cf7
simon <simonsingularitygroup@gmail.com>
2022-12-05 15:08:33 +0100

    use keyboard instead

--- env.py
+++ env.py
@@ -1,18 +1,11 @@
-import os
-import datetime
 import json
+import os
 
-PRESSED_KEYS = []
-KEY_PRESSED = None
 HOME = os.path.abspath(".")
 print(HOME)
 if not os.path.isdir(HOME):
     os.mkdir(HOME)
 
-LOGS_SESSION = datetime.datetime.now().replace(microsecond=0)
-LOGS_PATH = os.path.join(HOME, 'logs')
-if not os.path.isdir(LOGS_PATH):
-    os.mkdir(LOGS_PATH)
 KEY_MAP_PATH = os.path.join(HOME, 'key_map.json')
 if not os.path.isfile(KEY_MAP_PATH):
     with open(KEY_MAP_PATH, 'w+') as file:
